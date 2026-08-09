3c68159d50bc98bcbd33ce454cac523494834430
Esdras Garcia <esdrasgarcia.dev@gmail.com>
2026-08-09 07:52:45 -0300

    chore: initialize ReleaseOps project

---
+++ backend/src/main/java/dev/esdras/releaseops/ReleaseopsApplication.java
@@ -0,0 +1,13 @@
+package dev.esdras.releaseops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseopsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/esdras/releaseops/ReleaseopsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.esdras.releaseops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
